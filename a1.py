2bf23a95a8b340f43be42a2653ba35224e305496
SavageHippie302 <ewon0044@student.monash.edu>
2025-04-17 21:41:42 +1000

    created lookup table for where the friend is at each discrete time interval and dijkstras pseudo for later

--- a1.py
+++ a1.py
@@ -41,12 +41,52 @@ def intercept(roads, stations, start, friendStart):
     # Get index of friendStart
     friendStartIndex = stationIndex[friendStart]
 
-    # Total time for one train loop (NOTE: i think it's needed)
+    # Total time for one train loop
     trainLoopTime = 0
     for i, time in stations:
         trainLoopTime += time
     print(trainLoopTime)
+
+    friendPos = [None] * trainLoopTime
+
+    # time starts at 0, start index is where friend is starting
+    currentTime = 0
+    currentIndex = friendStartIndex
+
+    # Record initial location of friend
+    friendPos[0] = stations[friendStartIndex][0]
     
+    # calculate 1 loop of friend for lookup table of where the friend is at each point in time
+    for i in range(len(stations)):
+        # variable: time to the next station (station  =[next station, time])
+        timeNext = stations[currentIndex][1]
+
+        currentTime += timeNext
+        # increase current index 
+        currentIndex = (currentIndex + 1)
+
+        if currentTime < trainLoopTime:
+            friendPos[currentTime] = stations[currentIndex][0]
+        print(friendPos)
+    
+    '''
+    Dijkstras pseudocode (from week 5 slides FIT2004)
+    function dijkstra([V, E], s)
+        dist[1..n] = inf
+        pred[1..n] = 0
+        dist[s] = 0
+        Q = prio_q(V[1..n], key(v) = dist[v])
+        
+        while Q is not empty:
+            u = Q.pop_min()
+            for each edge e that is adjacent to u:
+            relax(e)
+        return dist[1..n], pred[1..n]
+    '''
+
+
+
+
 
 roads = [(6,0,3,1), (6,7,4,3), (6,5,6,2), (5,7,10,5), (4,8,8,5), (5,4,8,2),
 (8,9,1,2), (7,8,1,3), (8,3,2,3), (1,10,5,4), (0,1,10,3), (10,2,7,2),
